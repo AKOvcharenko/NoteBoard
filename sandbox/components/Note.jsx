a461b5acd8c9b0588dc3dfac5d86be6e0de49698
artem ovcharenko <akovcharenko"gmail.com>
2017-03-11 13:19:52 +0100

    single template

--- sandbox/components/Note.jsx
+++ sandbox/components/Note.jsx
@@ -3,31 +3,11 @@ import './../styles/note.scss';
 
 class Note extends Component{
     render(){
-
         return (
-            <div>
                 <div className="note">
                     <h2>Title #1</h2>
                     <p>Text Content #1</p>
                 </div>
-                <div className="note">
-                    <h2>Title #1</h2>
-                    <p>Text Content #1</p>
-                </div>
-                <div className="note">
-                    <h2>Title #1</h2>
-                    <p>Text Content #1</p>
-                </div>
-                <div className="note">
-                    <h2>Title #1</h2>
-                    <p>Text Content #1</p>
-                </div>
-                <div className="note">
-                    <h2>Title #1</h2>
-                    <p>Text Content #1</p>
-                </div>
-            </div>
-
         )
     }
 }
