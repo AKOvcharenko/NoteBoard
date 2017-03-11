73d9f6e5aa11e8bcf4ba34f5b0d4ca40e1251cdc
artem ovcharenko <akovcharenko"gmail.com>
2017-03-11 01:49:00 +0100

    mock note is ready

--- sandbox/components/Board.jsx
+++ sandbox/components/Board.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import Note from './Note.jsx';
 import './../styles/board.scss';
 
 class Board extends Component{
     render(){
         return (
                 <div className="board container-fluid">
-                    <div className="priority-urgent column col-sm-4 col-xs-12"></div>
+                    <div className="priority-urgent column col-sm-4 col-xs-12">
+                        <Note/>
+                        <Note/>
+                        <Note/>
+
+                    </div>
                     <div className="priority-normal column col-sm-4 col-xs-12"></div>
                     <div className="priority-low column col-sm-4 col-xs-12"></div>
                 </div>
---
+++ sandbox/components/Note.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './../styles/note.scss';
+
+class Note extends Component{
+    render(){
+
+        return (
+            <div>
+                <div className="note">
+                    <h2>Title #1</h2>
+                    <p>Text Content #1</p>
+                </div>
+                <div className="note">
+                    <h2>Title #1</h2>
+                    <p>Text Content #1</p>
+                </div>
+                <div className="note">
+                    <h2>Title #1</h2>
+                    <p>Text Content #1</p>
+                </div>
+                <div className="note">
+                    <h2>Title #1</h2>
+                    <p>Text Content #1</p>
+                </div>
+                <div className="note">
+                    <h2>Title #1</h2>
+                    <p>Text Content #1</p>
+                </div>
+            </div>
+
+        )
+    }
+}
+
+export default Note;
