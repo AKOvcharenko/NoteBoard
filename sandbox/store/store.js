6d5b4e2288178997fbf3f8a2f0d765cb6ba3ba5d
artem ovcharenko <akovcharenko"gmail.com>
2017-03-17 00:30:21 +0100

    debounce added

--- sandbox/store/store.js
+++ sandbox/store/store.js
@@ -1,7 +1,8 @@
-import {combineReducers, createStore} from 'redux';
-import { loadState, saveState } from './../own-modules/localStorage'
-import { noteState } from './reducers/reducerNotesState.js';
+import { loadState, saveState } from './../own-modules/localStorage';
 import { filtersState } from './reducers/reducerFiltersState.js';
+import { noteState } from './reducers/reducerNotesState.js';
+import {combineReducers, createStore} from 'redux';
+import debounce from 'debounce';
 
 const reducers = combineReducers({
     noteState,
@@ -12,7 +13,7 @@ const persistedState = loadState();
 
 const store = createStore(reducers, persistedState);
 
-store.subscribe(() => saveState({noteState: store.getState().noteState}));
+store.subscribe(debounce(() => saveState({noteState: store.getState().noteState}), 1000));
 
 window.store = store;
 
